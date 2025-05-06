93c759ec3e034f8966393035f45e74e1dc4fc85d
AdrianCamping <Oroliosglez@gmail.com>
2025-05-06 11:11:47 -0700

    in process of fixing example 2

---
+++ example2.asm
@@ -0,0 +1,34 @@
+############################ Data segment ############################
+.data
+    valf: .word 0
+    valg: .word 20
+    valh: .word 30
+    vali: .word 
+    valj: .word 
+############################ Code segment ############################
+.text
+.globl main
+
+main:
+    lw $t0, valf
+    lw $t1, valg
+    lw $t2, valh
+
+    lw $t3, vali
+    lw $t4, valj
+
+    slt $t5, $t3, $t4
+    bne $t5, $zero, false
+    add $t0, $t1, $t2
+    j exit
+false:
+    sub $t0, $t1, $t2
+exit:
+
+sw $t0, valf
+li $v0, 1
+move $a0, $t0
+syscall
+
+li $v0, 10
+syscall
